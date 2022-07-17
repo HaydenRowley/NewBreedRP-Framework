96905dc61702cd75d9de2289ef3b73a64aeb743e
Hayden <you@example.com>
2022-07-18 00:48:01 +0100

    come back to these

--- nb-core/commands/sv_commands.lua
+++ nb-core/commands/sv_commands.lua
@@ -4,16 +4,34 @@ TriggerEvent('NB-Base:addGroupCommand',  'setgroup', 'admin', function(source, a
     local player = NB.Functions.getPlayer(target)
     if target ~= nil then
         if player then
-           if NB.UserGroups[group] then
-            NB.Functions.setGroup(player, group)
-            -- add notification
-           else
-            -- add notification
-           end
+            if NB.UserGroup[group] then
+                NB.Functions.setGroup(player, group)
+                TriggerClientEvent('okokNotify:Alert', player, "Success", "Added person to the group!", 5000, 'success')
+            else
+                -- Notifications
+            end
         else
-            --add notification
-        end 
+            -- Notifications
+        end
+    end
     end, function(source, args, user)
-        -- No perm notification
+        -- Notifications
+end)
+
+
+TriggerEvent('NB-Base:addGroupCommand', 'console', 'admin', function(source, args, user)
+    local msg = args[1]
+    TriggerClientEvent("chatMessage", -1, "CONSOLE: " .. nessage, 3)
+end, function(source, args, user)
+    -- add notification
+end)
+
+TriggerEvent('MP-Base:addGroupCommand', 'EditPrio', 'admin', function(source, args, user)
+    local Player = MP.Functions.getPlayer(tonumber(args[1]))
+    local level = tonumber(args[2])
+    if Player ~= nil then
+        UpdatePriority(tonumber(args[1]), level)
+    else
+        -- Notifications
     end
 end)
